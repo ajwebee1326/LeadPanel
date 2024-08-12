dc5fd1a43f8f1d35f423bd6c4050ed46ebe5bce4
ajwebee1326 <ajay.kumar@webeesocial.com>
2024-08-12 16:03:55 +0530

    initial commit

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'aditya_birla');
+define('APP_NAME', 'My App');
+define('APP_URL', 'http://localhost/soft-ui');
+define('APP_EMAIL', 'default@gmail.com');
+define('APP_PHONE', '1234567890');
+define('APP_ADDRESS', '123, Street, City, Country');
+define('COPYRIGHT', 'Webeesocial');
+define('LOGIN_URL', 'login.php');
+date_default_timezone_set('Asia/Kolkata');
+define('PASSWORD_ALGO', PASSWORD_DEFAULT);
+?>
---
+++ conn.php
@@ -0,0 +1,43 @@
+<?php
+include 'config.php';
+session_start();
+$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+function sanatize($data, $flag = null){
+    global $conn;
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    if($flag == 'email'){
+        $data = filter_var($data, FILTER_SANITIZE_EMAIL);
+    }elseif($flag == 'int'){
+        $data = filter_var($data, FILTER_SANITIZE_NUMBER_INT);
+    }elseif($flag == 'float'){
+        $data = filter_var($data, FILTER_SANITIZE_NUMBER_FLOAT);
+    }
+    return $conn->real_escape_string($data);
+}
+
+function alert($msg){
+    echo '<script>alert("'.$msg.'")</script>';
+}
+
+function redirect($url){
+    echo '<script>window.location.href="'.$url.'"</script>';
+}
+
+function logout(){
+    session_destroy();
+    redirect(APP_URL);
+}
+
+function validae_login(){
+    if(!isset($_SESSION['user'])){
+        redirect(LOGIN_URL);
+    }
+}
+
+?>
